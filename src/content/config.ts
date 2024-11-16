8998c2cc9285203f397508302a0f5f3211b6e462
Stephanie O'Hanley <stephohanley@gmail.com>
2024-11-16 02:06:35 -0500

    Code corrections/updates

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,30 +10,33 @@ const blog = defineCollection({
     updatedDate: z.coerce.date().optional(),
     description: z.string().optional(),
     author: z.string().optional(),
+    authorUrl: z.string().url().optional(),
     authorPic: z.string().optional(),
     deets: z.any().optional(),
-    commentStatus:z.string().optional(),
+    commentStatus: z.string().optional(),
     responses: z.string().optional(),
     responsesNumber: z.string().optional(),
     heroImage: z.string().optional(),
     tags: z.array(z.string()),
     categories: z.array(z.string()).optional(),
     relatedPosts: z.array(reference("blog")).optional(),
-    postComments: z.array(z.object({
-      commenterName: z.string().optional(),
-      authorUrl: z.string().url().optional(),
-      date: z.string().optional(),
-      html: z.string().optional(),
-      authorAvatar: z.string().optional(),
-      authorAvatarUrls: z.string().optional(),
-    })).optional(),
+    postComments: z
+      .array(
+        z.object({
+          commenterName: z.string().optional(),
+          date: z.string().optional(),
+          html: z.string().optional(),
+          authorAvatar: z.string().optional(),
+          authorAvatarUrls: z.string().optional(),
+        }),
+      )
+      .optional(),
     contributors: z.array(reference("contributors")).optional(),
   }),
 });
 
-
 const commentsCollection = defineCollection({
-  type: 'data',
+  type: "data",
   schema: z.object({
     id: z.number().optional(),
     parent: z.string().nullable().optional(),
@@ -49,9 +52,8 @@ const commentsCollection = defineCollection({
 const contributors = defineCollection({
   type: "content",
   schema: z.object({
-    contributor:z.string().optional(),
+    contributor: z.string().optional(),
     contributorUrl: z.string().optional(),
-    
   }),
 });
 
